8af235bf06f33e58e205ce94b050b1eab9301118
RSkala <RichardSkala@gmail.com>
2024-05-13 18:32:46 -0700

    PlayerController script creation

---
+++ ShootEmUpTutorial-Proj/Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shmup
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+        
+        void Update()
+        {
+        
+        }
+    }
+}
